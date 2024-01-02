3dd1fb88dd162964c523792c7531b709541a37ba
Akash A <akash@scube.co>
2024-01-02 11:58:06 +0530

    updated

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -4,32 +4,43 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 export default function ProductForm({
+  _id,
   title: currentTitle,
   description: currentDescription,
   price: currentPrice,
+  images,
 }) {
   const [title, setTitle] = useState(currentTitle || "");
   const [description, setDescription] = useState(currentDescription || "");
   const [price, setPrice] = useState(currentPrice || "");
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
-  // useEffect(()=>{
-  //   setTitle(currentTitle || "");
-  //   setDescription(currentDescription || "");
-  //   setPrice(currentPrice || "");
-  // },[currentTitle,
-  //   currentDescription,
-  //   currentPrice])
   async function createProduct(event) {
     event.preventDefault();
     const data = { title, description, price };
-    await axios.post("/api/products", data);
+
+    if (_id) {
+      await axios.post("/api/products", { ...data, _id });
+    } else {
+      await axios.post("/api/products", data);
+    }
     setGoToProducts(true);
   }
   if (goToProducts) {
     router.push("/admin/products");
   }
-  console.log(currentTitle,currentDescription,currentPrice);
+
+  function uploadImages(event) {
+    console.log(event);
+    const files = event?.target?.files;
+    if (files.lenth > 0) {
+      const data = new FormData(); // Pass the data in backend value
+      files.forEach((file) => {
+        data.append("file", file);
+      });
+    }
+  }
+
   return (
     <form class="w-full max-w-xl add-product-form" onSubmit={createProduct}>
       <div class="md:flex md:items-center mb-6">
@@ -50,6 +61,36 @@ export default function ProductForm({
             onChange={(e) => setTitle(e.target.value)}
             value={title}
           />
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-full-name"
+          >
+            Product Images
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <label className="w-full h-24 border flex items-center justify-center gap-1 text-gray-500 rounded-md bg-gray-200 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+              />
+            </svg>
+            Upload
+            <input type="file" className="hidden" onChange={uploadImages} />
+          </label>
         </div>
       </div>
       <div class="md:flex md:items-center mb-6">
--- src/pages/admin/products.js
+++ src/pages/admin/products.js
@@ -1,20 +1,25 @@
 import AdminDashbordLayout from "@/components/Layout/AdminDashbordLayout";
+import Loading from "@/components/Loading/Loading";
 import ProductTable from "@/components/ProductTable/ProductTable";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
   const [productInfo, setProductInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios.get("/api/products").then((res) => {
       setProductInfo(res.data);
     });
+    setLoading(false);
   }, []);
 
   return (
     <>
       <AdminDashbordLayout>
+        {loading === true && <Loading />}
         <ProductTable data={productInfo} />
       </AdminDashbordLayout>
     </>
